8c81bdb31769be95cdebafb22494a0d9c6fd6802
Benjamin Morrell <BDMorrell@gmail.com>
2020-08-20 14:53:15 -0600

    Make interactive parser utility

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <string>
 #include "version.h"
+#include "parser/csvTokenizer.h"
+#include "parser/parseErrors.h"
 
 using namespace std;
+using namespace echeck::parser;
 
 int main(int arc, char **argv)
 {
-  cout << "Hello world!" << endl;
-  cout << "Version " << ElectronicCheckbook_VERSION_MAJOR << "." << ElectronicCheckbook_VERSION_MINOR << endl;
+  csvTokenizer ct(&cin);
+  int count = 0;
+  while (cin.good()) {
+    try {
+      string token;
+      token = ct.getField();
+      cout << count++ << ": " << token << endl;
+      int c = cin.peek();
+      if (c == '\n') {
+        cout << "NL" << endl;
+      } else {
+        cout << "Peek " << c << ": " << (char) c << endl;
+      }
+      if (c == '\n' || c == ',') {
+        ct.advanceInput();
+      }
+    } catch (parser_error e) {
+      cerr << "ERROR: " << e.what() << endl;
+    }
+  }
   return 0;
 }
+
